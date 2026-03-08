09436ff37bfa685bd3ed4ce39b0018244425474c
marlanz <phucanhdodang1211@gmail.com>
2026-03-08 23:56:39 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ const Home = async () => {
   return (
     <section>
       <h1 className="text-center">
-        The Hub for Every Dev <br /> Event You Cannot Miss
+        The Hub for Every Deva <br /> Event You Cannot Miss
       </h1>
       <p className="text-center mt-5">
         Hackathons, Meetups, and Conference, All in One Place
